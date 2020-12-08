dca563b12ecea3f63a048ae091f2b4cb3adf8985
Ethan Heil <eheil20@gmail.com>
2020-12-08 18:21:02 -0500

    Changing weather node to inherit from Particles. Starting volume initialization

--- Modules/WeatherSystem/WeatherSystemNode.cpp
+++ Modules/WeatherSystem/WeatherSystemNode.cpp
@@ -1,6 +1,12 @@
 #include "WeatherSystemNode.h"
 
- WeatherNode::WeatherNode() {
+WeatherNode::WeatherNode()
+{
+	mVolWidth = 8.0f;
+	mVolHeight = 8.0f;
+	mVolLength = 8.0f;
+
+	ConstructVolume();
 }
 
  WeatherNode::~WeatherNode() {
@@ -8,9 +14,46 @@
 
 void WeatherNode::ConstructVolume()
 {
+	set_visibility_aabb(AABB(Vector3(-4, -4, -4), Vector3(mVolWidth, mVolHeight, mVolLength)));
+}
+
+void WeatherNode::_notification(int p_what)
+{
+	switch (p_what) {
+		case NOTIFICATION_PROCESS:
+			//ConstructVolume();
+			break;
+	}
 }
 
 void WeatherNode::_bind_methods()
 {
+	ClassDB::bind_method(D_METHOD("ConstructVolume"), &WeatherNode::ConstructVolume);
+
+	ClassDB::bind_method(D_METHOD("IsRainActive"), &WeatherNode::IsRainActive);
+	ClassDB::bind_method(D_METHOD("IsSnowActive"), &WeatherNode::IsSnowActive);
+	ClassDB::bind_method(D_METHOD("WetCamera"), &WeatherNode::WetCamera);
+
+	ClassDB::bind_method(D_METHOD("SetRain", "rain"), &WeatherNode::SetRain);
+	ClassDB::bind_method(D_METHOD("SetSnow", "snow"), &WeatherNode::SetSnow);
+	ClassDB::bind_method(D_METHOD("SetWetCamera", "wetCamera"), &WeatherNode::SetWetCamera);
+
+	//ClassDB::bind_method(D_METHOD("GetAffectedCamera"), &WeatherNode::GetAffectedCamera);
+	//ClassDB::bind_method(D_METHOD("SetAffectedCamera", "camera"), &WeatherNode::SetAffectedCamera);
+
+	ClassDB::bind_method(D_METHOD("GetVolWidth"), &WeatherNode::GetVolWidth);
+	ClassDB::bind_method(D_METHOD("GetVolHeight"), &WeatherNode::GetVolHeight);
+	ClassDB::bind_method(D_METHOD("GetVolLength"), &WeatherNode::GetVolLength);
+
+	ClassDB::bind_method(D_METHOD("SetVolWidth", "width"), &WeatherNode::SetVolWidth);
+	ClassDB::bind_method(D_METHOD("SetVolHeight", "height"), &WeatherNode::SetVolHeight);
+	ClassDB::bind_method(D_METHOD("SetVolLength", "length"), &WeatherNode::SetVolLength);
+
+	ADD_PROPERTY(PropertyInfo(Variant::REAL, "mVolWidth", PROPERTY_HINT_NONE, "", PROPERTY_USAGE_EDITOR | PROPERTY_USAGE_STORAGE | PROPERTY_USAGE_CHECKABLE), "SetVolWidth", "GetVolWidth");
+	ADD_PROPERTY(PropertyInfo(Variant::REAL, "mVolHeight", PROPERTY_HINT_NONE, "", PROPERTY_USAGE_EDITOR | PROPERTY_USAGE_STORAGE | PROPERTY_USAGE_CHECKABLE), "SetVolHeight", "GetVolHeight");
+	ADD_PROPERTY(PropertyInfo(Variant::REAL, "mVolLength", PROPERTY_HINT_NONE, "", PROPERTY_USAGE_EDITOR | PROPERTY_USAGE_STORAGE | PROPERTY_USAGE_CHECKABLE), "SetVolLength", "GetVolLength");
+
+	ADD_PROPERTY(PropertyInfo(Variant::BOOL, "mRain", PROPERTY_HINT_NONE, "", PROPERTY_USAGE_EDITOR | PROPERTY_USAGE_STORAGE | PROPERTY_USAGE_CHECKABLE), "SetRain", "IsRainActive");
+	ADD_PROPERTY(PropertyInfo(Variant::BOOL, "mSnow", PROPERTY_HINT_NONE, "", PROPERTY_USAGE_EDITOR | PROPERTY_USAGE_STORAGE | PROPERTY_USAGE_CHECKABLE), "SetSnow", "IsSnowActive");
 
 }
--- Modules/WeatherSystem/WeatherSystemNode.h
+++ Modules/WeatherSystem/WeatherSystemNode.h
@@ -6,10 +6,9 @@
 #include "scene/3d/camera.h"
 #include "scene/3d/particles.h"
 #include "scene/3d/spatial.h"
-#include "core/reference.h"
 
-class WeatherNode : public Spatial {
-	GDCLASS(WeatherNode, Spatial)
+class WeatherNode : public Particles {
+	GDCLASS(WeatherNode, Particles)
 public:
 	WeatherNode();
 	~WeatherNode();
@@ -24,15 +23,24 @@ public:
 	void SetSnow(bool snowing) { mSnow = snowing; }
 	void SetWetCamera(bool wetCamera) { mWetCamera = wetCamera; }
 
-	Camera* GetAffectedCamera() const { return mCamera; }
-	void SetAffectedCamera(Camera* camera) { mCamera = camera; }
+	//Camera GetAffectedCamera() const { return mCamera; }
+	//void SetAffectedCamera(Camera camera) { mCamera = camera; }
+
+	float GetVolWidth() const { return mVolWidth; }
+	float GetVolHeight() const { return mVolHeight; }
+	float GetVolLength() const { return mVolLength; }
+
+	void SetVolWidth(float width) { mVolWidth = width; }
+	void SetVolHeight(float height) { mVolHeight = height; }
+	void SetVolLength(float length) { mVolLength = length; }
+
+	void _notification(int p_what);
 
 protected:
 	static void _bind_methods();
 
 private:
-	Particles mParticleNode;
-	Camera* mCamera; //Camera affected by the WeatherNode
+	//Camera mCamera = Camera(); //Camera affected by the WeatherNode
 
 	float mVolWidth;
 	float mVolLength;
